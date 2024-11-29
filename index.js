f542e9e6771a0f449ec495038873bc65d62ff8d1
CARLOS LEONARDO MARIANO <carloslmariano2000@gmail.com>
2024-11-29 15:51:59 -0300

    ok

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-const mobileBtn = document.getElementById('mobile-btn');
-const mobileMenu = document.getElementById('mobile_menu');
-const remover_header = document.getElementById('remover_header')
-const backdrop = document.getElementById('backdrop')
-
-
-
-// Ao clicar no botão de abrir o menu
-
-mobileBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active'); // Alterna a classe 'active' no mobile_menu
-    mobileBtn.style.display = "none" // Oculta o botão de abrir
-    backdrop.style.display = "block" // Exibe o backdrop
-    document.body.style.cursor = "pointer" // Define o cursor como "pointer" em qualquer lugar da tela
-
-
+document.getElementById("mobile-btn").addEventListener("click", function() {
+    document.getElementById("mobile_menu").classList.toggle("active");
+    document.getElementById("backdrop").style.display = "block";  // Exibe o fundo semitransparente
+    document.getElementById("mobile-btn").style.display = "none"
 });
 
-// Ao clicar no botão de fechar o menu
-
-remover_header.addEventListener('click', () => {
-    mobileMenu.classList.remove('active') // Fecha o menu removendo a classe 'active'
-    backdrop.style.display = "none" // Oculta o backdrop
-    mobileBtn.style.display = "block" // Exibe o botão de abrir novamente
-    document.body.style.cursor = "default" // Volta ao cursor padrão
+document.getElementById("remover_header").addEventListener("click", function() {
+    document.getElementById("mobile_menu").classList.remove("active");
+    document.getElementById("backdrop").style.display = "none";  // Oculta o fundo
+    document.getElementById('mobile-btn').style.display = "block"
 });
-
-
-// Fechar o menu se clicar fora dele
-document.addEventListener('click', (event) => {
- // Verifica se o clique foi fora do menu e do botão de abrir
- if (!mobileMenu.contains(event.target) && !mobileBtn.contains(event.target)) {
-    mobileMenu.classList.remove('active'); // Fecha o menu
-    backdrop.style.display = "none"; // Oculta o backdrop
-    mobileBtn.style.display = "block"; // Exibe o botão de abrir novamente
-    document.body.style.cursor = "default"; // Volta ao cursor padrão
-}});
-
-
-
