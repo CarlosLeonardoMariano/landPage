dc75f53fe1fb8bea3004eaa54048f08c8c1af400
CARLOS LEONARDO MARIANO <carloslmariano2000@gmail.com>
2024-12-02 18:52:43 -0300

    ok

--- index.js
+++ index.js
@@ -11,4 +11,68 @@ document.getElementById("remover_header").addEventListener("click", function() {
 });
 
 
+const home = document.querySelector('.home');
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((procurar) => {
+        if (procurar.isIntersecting) {
+            procurar.target.classList.add('show');
+        } else {
+            procurar.target.classList.remove('show');
+        }
+    });
+});
+
+myObserver.observe(home);
+
+
+const navItens = document.querySelector('.header');
+
+const header = new IntersectionObserver( (entries) => {
+    entries.forEach( (procurar) => {
+        if(procurar.isIntersecting) {
+            procurar.target.classList.add('show2')
+        } else {
+            procurar.target.classList.remove('show2')
+        }
+    })
+
+})
+
+header.observe(navItens)
+
+
+        const h2_menu = document.getElementById('h2_menu')
+
+        const textH2 = new IntersectionObserver( (entries) => {
+            entries.forEach( (procurar)=> {
+                if(procurar.isIntersecting){
+                    procurar.target.classList.add('show4')
+                } else {
+                    procurar.target.classList.remove('show4')
+                }
+
+            })
+        })
+
+textH2.observe(h2_menu)
+
+
+const testeavaliacao = document.querySelector('.testeavaliacao')
+
+const comentarios = new IntersectionObserver( (entries) => {
+    entries.forEach( (procurar) => {
+        if(procurar.isIntersecting){
+            procurar.target.classList.add('show6')
+        } else {
+            procurar.target.classList.remove('show6')
+
+        }
+    })
+})
+
+comentarios.observe(testeavaliacao)
+
+
+
 
